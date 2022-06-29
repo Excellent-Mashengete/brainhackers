b4c4f6c8f945e0e6ad310f9fc34e8b0e0d7d3b21
mapsKay <kwenamapitso@gmail.com>
2022-06-29 16:28:40 +0200

    second commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ProductsComponent } from './Components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
