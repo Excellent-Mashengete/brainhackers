e8a2b1dda3825e286e01c3eda1d8c6cf00ad8ef4
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2022-07-04 16:36:59 +0200

    finalfrontend

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FinalProductsPageComponent } from './final-products-page/final-products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinalProductsPageComponent
   ],
   imports: [
     BrowserModule,
