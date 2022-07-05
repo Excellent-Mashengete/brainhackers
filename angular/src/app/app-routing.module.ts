9db45c1e114d72d167ab0a933ad6cc53acd78edb
adelaide20 <adelaiderorisang@gmail.com>
2022-07-05 12:17:46 +0200

    latest changing, UI started

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './Components/cart/cart.component';
 import { FinalProductsPageComponent } from './Components/final-products-page/final-products-page.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProductComponent } from './Components/product/product.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'login', component:LoginComponent},
-  {path:'product', component:ProductComponent},
-  {path:'cart',component:CartComponent},
-  {path:'products',component:FinalProductsPageComponent},
-  {path:'products/:id',component:ProductComponent},
   {path:'register', component : RegisterComponent},
-
+  {path:'products',component:FinalProductsPageComponent},
 ]
 
 @NgModule({
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,20 +6,14 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FinalProductsPageComponent } from './Components/final-products-page/final-products-page.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
-import { CartComponent } from './Components/cart/cart.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { ProductComponent } from './Components/product/product.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
-    HeaderComponent,
-    ProductComponent,
     RegisterComponent,
     FinalProductsPageComponent,
     NavBarComponent,
