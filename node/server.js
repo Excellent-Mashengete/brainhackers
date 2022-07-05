618404f532191c30b593118d8ea6f9a7a382b6d4
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-07-05 16:25:19 +0200

    merge with register

--- node/server.js
+++ node/server.js
@@ -9,18 +9,18 @@ const bodyParser = require('body-parser');
 
 const dbConnect = require('../node/configurations/dbconn');                 // connecting to database database
 
-const port = process.env.PORT || 4201;                                  // the port
+const port = process.env.PORT || 4202;                                  // the port
 
 app.use( express.json() )
 
 app.use( express.urlencoded({ extended: true }) )                     //  sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body 
 
-app.use('/products', require('./routes/product_route'))                   // calling the routes 
+//app.use('/products', require('./routes/product_route'))                   // calling the routes 
 
 
-// app.get('/', (req, res) =>{
-//    res.send("welcome");
-// })
+ app.get('/', (req, res) =>{
+  res.send("welcome");
+ })
 
 
 
