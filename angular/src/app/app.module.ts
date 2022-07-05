0383e0367cd3b8d4e084463edc80b8f024e9c212
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2022-07-05 10:00:50 +0200

    navbar and a product page with html and scss

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FinalProductsPageComponent } from './final-products-page/final-products-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FinalProductsPageComponent
+    FinalProductsPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
