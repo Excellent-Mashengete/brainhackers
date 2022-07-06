ffbef2c44c5e27a70dd7991d5903f3a87b1c2742
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2022-07-06 16:31:27 +0200

    updaing proucts

--- node/server.js
+++ node/server.js
@@ -6,36 +6,17 @@ const app = express();
 
 
 const bodyParser = require('body-parser');
+// const dbConnect = require('../node/configurations/productsconfig');                 // connecting to database database
 
-const dbConnect = require('../node/configurations/dbconn');                 // connecting to database database
 
-const port = process.env.PORT || 4201;                                  // the port
+const port = process.env.PORT || 3000;                                  // the port
 
 app.use( express.json() )
 
 app.use( express.urlencoded({ extended: true }) )                     //  sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body 
 
-app.use('/products', require('./routes/product_route'))                   // calling the routes 
 
-
-// app.get('/', (req, res) =>{
-//    res.send("welcome");
-// })
-
-
-
-//catch all errors that are not handled
-
-// process.on('uncaughtException', (error)  => {
-//     console.log('Alert! ERROR : ',  error);
-//     process.exit(1);                                                       // Exit your app 
-//  })
-
-//  process.on('unhandledRejection', (error, promise)  => {
-//     console.log('Alert! ERROR : ',  error);
-//     process.exit(1); 
-//  })
-
+require('./routes/productsroutes')(app)
 
  
 app.listen(port, () => 
