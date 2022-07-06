a56e62fdeb8dd56f35f6df95ff7493e0770e50fe
Fhatuwani <fhatuwani.raphalalani@dadevs.co.za>
2022-07-06 16:11:16 +0200

    added backend to cart

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { FinalProductsPageComponent } from './Components/final-products-page/final-products-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-=======
-import { FinalProductsPageComponent } from './components/final-products-page/final-products-page.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
->>>>>>> 5bf18e1b4d207a74583cf348f9ce195cf11e80f3
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
