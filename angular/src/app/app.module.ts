76e409d132623f9143194f8cf8baba34fab1fd2b
adelaide20 <adelaiderorisang@gmail.com>
2022-07-08 10:37:45 +0200

    latest changes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,19 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import{AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './components/register/register.component';
-
-
-
-
-import { HttpClientModule } from '@angular/common/http';
-// import { LoginComponent } from './components/login/login.component';
-// import { RegisterComponent } from './components/register/register.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-// import { FinalProductsPageComponent } from './components/final-products-page/final-products-page.component';
-// import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-
 import { LoginComponent } from './components/login/login.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FinalProductsPageComponent } from './Components/final-products-page/final-products-page.component';
