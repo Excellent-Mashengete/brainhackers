ebf78c1d0d4f1e5a7014b181c7a7142758487e49
DA_M1_56 <da_m1_56@DAs-MacBook-Air.local>
2022-07-11 12:59:19 +0200

    initial

--- angular/src/app/Services/card.service.ts
+++ angular/src/app/Services/card.service.ts
@@ -5,10 +5,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CardService {
-  baseUrl = ""
+
+  //BACKEND URL HERE/API
+  baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&language=en-US&primary_release_date.gte=2022-09-15&primary_release_date.lte=2024-10-22"
 
   constructor ( private http : HttpClient) { }
+  
 
+//THIS FUCTION HELPS US GET THE URL USING HTTP CLIENT
 
   getCard(){
     return this.http.get(this.baseUrl);
