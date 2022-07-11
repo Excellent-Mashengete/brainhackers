3379f97065a44350bbc3d6f804383db9060ecf4b
Mapitso <mapitso.kgowana@dadevs.co.za>
2022-07-11 12:11:45 +0200

    first commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FinalProductsPageComponent } from './Components/final-products-page/final-products-page.component';
 import { OneProductComponent } from './Components/one-product/one-product.component';
+import { SignupComponent } from './controllers/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { OneProductComponent } from './Components/one-product/one-product.compon
     LoginComponent,
     ShoppingCartComponent,
     NavBarComponent,
-    OneProductComponent
+    OneProductComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
