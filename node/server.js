e103404697ebf6322f87c2b730b106a61f1c07f5
DA M1 36 <dam136@MAC-M1-32.local>
2022-07-11 10:29:26 +0200

    first commit working backend

--- node/server.js
+++ node/server.js
@@ -1,67 +1,51 @@
-//modules
-require('dotenv').config;
-const express = require('express');
-const app = express();
-const cors = require("cors");
-
-const cors = require('cors') //initialising cors 
-const bodyParser = require('body-parser');
-// const dbConnect = require('../node/configurations/productsconfig');                 // connecting to database database
-
-const dbConnect = require('../node/configurations/dbconn');
-const dbConnect = require('../node/controllers/productController'); // connecting to database database
-const port = process.env.PORT || 4201; // the port
-
+const express = require("express"); // import express library
+const cors = require("cors"); //import cors module
+const app = express(); //Initialize express
 var corsOptions = {
-    origin: "http://localhost:8081"
-};
+  //origin: "http://localhost:4200"
+  origin: "*"
+};// only allow that listerning address to connnect to the database
 
-app.use(express.json())
+const bodyParser = require('body-parser');
+require("./App/config/dotenv.config"); //Import your environmental configs
+const client = require ("./App/config/database.config");
 
-app.use(express.urlencoded({ extended: true })) //  sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body 
+//const user = require("./App/routes/routers")
 
-//==================================== calling the routes======================= 
-app.use('/', require('./routes/product_route'))
+app.use(express.json());  // to support JSON-encoded
+app.use(cors(corsOptions));
 
-//================================== using cors ===============================
-// app.use( cors({                                                   
-//    origin: 'http://localhost:4200'
-// }) )     
+app.use(express.urlencoded({ extended: true }));
+// simple route  // to support JSON-encoded 
+// app.use( cors({origin: true, credentials: true}) )
+ 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 
-// //=====================================using cors for all =======================
-// app.use( cors({                                                     // using cors 
-//    origin: '*'
-// }) ) 
+const port = process.env.PORT || 7070;
+ 
+const hostURL = '0.0.0.0'; //Fault-Tolerant listening port for Backend. Picks available dynamic IPv4 and IPv6 addresses of the local host
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200D"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+client.connect((err) =>{ // Connect to the Database
+   if (err) {
+      console.log(err)
+     }
+  else {
+    console.log("Data logging initialised");
+   }
 });
 
-// app.get('/', (req, res) =>{
-//    res.send("welcome");
-// })
+app.get("/", (req, res) =>{
+    res.status(200).send("Sever Initialized and Online. Ready to take OFF!");
+});
 
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
+//app.use("/api", user) // User endpoint API
 
-app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-
-app.use(express.urlencoded({ extended: true })) //  sending data (in the form of some data object) to the server and you are asking the server to accept or store that data (object), which is enclosed in the body 
-
-app.use('/products', require('./routes/product_route')) // calling the routes 
-
-// app.get("/", (req, res) => {
-//    res.json({ message: "Welcome to bezkoder application." });
-//  });
-
-require('./routes/productsroutes')(app)
-
-app.listen(port, () =>
-    console.log(`API running on localhost:${port}`)
-);
+app.listen(port, process.env.baseURL , () =>{  
+   console.log(`Here we go, All Engines started at ${port}.`) 
+})
