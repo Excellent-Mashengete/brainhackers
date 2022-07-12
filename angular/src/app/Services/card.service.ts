f650d140a0de094a550d1737eeb08e2a6848d52f
joachim <joachim.mhlongo@thedigitalacademy.co.za>
2022-07-12 09:59:18 +0200

    each

--- angular/src/app/Services/card.service.ts
+++ angular/src/app/Services/card.service.ts
@@ -8,7 +8,7 @@ export class CardService {
 
   //BACKEND URL HERE/API
   //baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&language=en-US&primary_release_date.gte=2022-01-15&primary_release_date.lte=2022-05-22"
-  baseUrl="http://localhost:7070/api/"
+  baseUrl="http://localhost:7070/api/product_list"
   constructor ( private http : HttpClient) { }
   
 
